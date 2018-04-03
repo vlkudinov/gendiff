4fc2697b4a3a0bcd380056df7e53a023b1a2e268
Vladimir Kudinov <vlkudinov@gmail.com>
2018-04-03 14:55:34 +0300

    step #4

--- __tests__/test.js
+++ __tests__/test.js
@@ -6,7 +6,7 @@ const dir = '__tests__/__fixtures__';
 test('compare json', () => {
   const path1 = `${dir}/json/before.json`;
   const path2 = `${dir}/json/after.json`;
-  const expected = fs.readFileSync(`${dir}/json/expected.json.txt`, 'utf8');
+  const expected = fs.readFileSync(`${dir}/expected.txt`, 'utf8');
   const current = gendiff(path1, path2);
   expect(current).toBe(expected);
 });
@@ -14,7 +14,7 @@ test('compare json', () => {
 test('compare yaml', () => {
   const path1 = `${dir}/yaml/before.yaml`;
   const path2 = `${dir}/yaml/after.yaml`;
-  const expected = fs.readFileSync(`${dir}/yaml/expected.yaml.txt`, 'utf8');
+  const expected = fs.readFileSync(`${dir}/expected.txt`, 'utf8');
   const current = gendiff(path1, path2);
   expect(current).toBe(expected);
 });
@@ -22,7 +22,15 @@ test('compare yaml', () => {
 test('compare yml', () => {
   const path1 = `${dir}/yaml/before.yml`;
   const path2 = `${dir}/yaml/after.yml`;
-  const expected = fs.readFileSync(`${dir}/yaml/expected.yaml.txt`, 'utf8');
+  const expected = fs.readFileSync(`${dir}/expected.txt`, 'utf8');
+  const current = gendiff(path1, path2);
+  expect(current).toBe(expected);
+});
+
+test('compare ini', () => {
+  const path1 = `${dir}/ini/before.ini`;
+  const path2 = `${dir}/ini/after.ini`;
+  const expected = fs.readFileSync(`${dir}/expected.txt`, 'utf8');
   const current = gendiff(path1, path2);
   expect(current).toBe(expected);
 });
